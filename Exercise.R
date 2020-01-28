da250a62fec856abb05bb8f596dd1e173321bb90
danielsk123 <60381046+danielsk123@users.noreply.github.com>
2020-01-28 16:29:25 +0200

    Exercise 4.5 and Afternoon session

---
+++ Exercise.R
@@ -0,0 +1,37 @@
+#Exercise 4.5
+#Day_1
+#Kim Daniels
+#28_01_2020
+
+
+#Importing laminaria.csv
+library(readr)
+laminaria <- read_delim("data/laminaria.csv", 
+                        ";", escape_double = FALSE, trim_ws = TRUE)
+
+#Create a new data frame from the laminaria dataset that meets the following criteria: contains only the site column and a new column called total_length_half containing values that are half of the total_length. In this total_length_half column, there are no NAs and all values are less than 100.
+
+New_dataframe<-laminaria %>% 
+  mutate(total_length_half=total_length/2) %>% 
+  select(site,total_length_half(na.rm=TRUE))
+
+#Find the mean, min, and max blade_length for each site. Also add the number of observations
+        
+laminaria %>%
+  group_by(site,blade_length)%>%
+  summarise(mean_blade_length=mean(blade_length),
+            min_blade_length=min(blade_length),
+            max_blade_length=max(blade_length),n=n())
+
+#What was the heaviest stipe measured in each site?
+laminaria %>%
+  group_by(site) %>%
+  summarise(max_stipe_mass=max(stipe_mass))
+
+#Return the columns site, region, and stipe_length.
+laminaria %>%
+  select(site, region, stipe_length, stipe_mass)
+
+###
+    
+            
