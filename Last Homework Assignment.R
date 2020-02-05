d46d2149efc0e213026624d4834769fab730c8a8
danielsk123 <34078862@myuwc.ac.za>
2020-02-05 13:32:43 +0200

    Last Homework Assignment Aamirah Botha (3440074) and Kim Daniels (3407862) R script and Map (pdf) attached

--- Last Homework Assignment.R
+++ Last Homework Assignment.R
@@ -1,4 +1,3 @@
-#Day 4
 #Last Homework Assignment
 #Kim Daniels and Aamirah Botha
 #Due: 5 February 2020
@@ -11,6 +10,46 @@ library(readr)
 library(scales)
 library(ggsn)
 library(maps)
+library(ggplot2)
 
-bathywide_tidy <- bathy_wide %>%
-  gather(long, key = "lat")
+
+#Importing Data
+
+load("C:/Intro_R_2020/data/gebco_sa.Rdata")
+
+
+#Tidy Data
+bathy_wide_tidy <- gather(data=bathy_wide, key="lon",
+                          value="elevation",-lat)
+
+
+#Converting Data to Numeric as Dataframe
+
+#bathy_wide_2<- as.numeric(bathy_wide_tidy)
+
+#bathy_wide_2<- sapply(bathy_wide_converted, as.numeric)
+
+bathy_wide_2 <- as.data.frame(apply(bathy_wide_tidy, 2, as.numeric))
+
+
+#View Data
+head (bathy_wide_2)
+
+
+#Mapping
+ggplot(data = bathy_wide_2, aes(x = lon, y = lat)) + #Data and axes
+geom_raster(aes(fill = elevation))+ #Adding elevation values to the map
+  scale_fill_gradientn("Elevation (m)",values=scales::rescale(c(-6129,0,1,3374)),
+                    colors=c("navyblue", "cyan1", "darkgreen","darkolivegreen",  "burlywood1"))+ #Adding colour and scale
+  scale_x_continuous(breaks= seq(10,35,5),
+                     labels = c("10", "15", "20", "25" , "30", "35"),
+                     position= "bottom")+ #Longitude scale
+  scale_y_continuous(expand= c(0,0))+ #Latitude Scale
+theme(panel.grid.major= element_blank(), panel.grid.minor = element_blank(),
+      panel.background = element_blank())+ #Crops grid to map
+  scale_fill_manual(elevation(c(1000)),color="darkolivegreen") #Attempts to add elevation of 1000 in colour dark olive green
+
+####
+
+
+
